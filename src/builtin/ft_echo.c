48a37d2a64ddc0588ab349d3c5795e7049990f93
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-02 12:53:18 +0800

    Add files via upload

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -6,49 +6,12 @@
 /*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 13:00:06 by abasarud          #+#    #+#             */
-/*   Updated: 2023/04/18 13:50:05 by abasarud         ###   ########.fr       */
+/*   Updated: 2023/05/02 12:50:36 by abasarud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../libft/libft.h"
-
-/*int	echo(int argc, char **argv, t_token curr)
-{
-	int	i;
-	int	flag;
-
-	if (argc < 2)
-		return (1);
-	i = 1;
-	flag = 0;
-	if (argv[i] && strncmp(argv[i], "-n", 2) == 0)
-	{
-		flag = 1;
-		i++;
-	}
-	if (curr.quote != 1)
-	{
-		printf("str %s", argv[i]);
-		argv[i] = ft_delete_quote(argv[i]);
-		show_var(argv[i], head_ref);
-	}
-	else
-	{
-		while (argv[i])
-		{
-			if (closed_quote(argv[i]) || closed_dub_quote(argv[i]))
-				argv[i] = ft_delete_quote(argv[i]);
-			printf("%s", argv[i]);
-			if (argv[i + 1])
-				printf(" ");
-			i++;
-		}
-	}
-	if (flag == 0)
-		printf("\n");
-	return (0);
-}*/
 
 void	print_echo_args(char **argv, int start)
 {
@@ -66,6 +29,20 @@ void	print_echo_args(char **argv, int start)
 	}
 }
 
+#include <stdbool.h>
+
+int has_dollar_sign(const char *str)
+{
+    const char *ptr = str;
+    while (*ptr != '\0')
+	{
+        if (*ptr == '$')
+            return 1;
+        ptr++;
+    }
+    return 0;
+}
+
 int	echo(int argc, char **argv, t_token curr)
 {
 	int	i;
@@ -75,21 +52,31 @@ int	echo(int argc, char **argv, t_token curr)
 		return (1);
 	i = 1;
 	flag = 0;
-	if (argv[i] && strncmp(argv[i], "-n", 2) == 0)
+	if (argv[i] && ft_strncmp(argv[i], "-n", 2) == 0)
 	{
 		flag = 1;
 		i++;
 	}
-	if (curr.quote != 1)
+	if (curr.quote == 2 && ft_dollar(argv[i]))
 	{
-		printf("str %s", argv[i]);
 		argv[i] = ft_delete_quote(argv[i]);
+		if (ft_dollar(argv[i]))
+			argv[i] = strremove(argv[i], "$");
 		show_var(argv[i], head_ref);
+		printf("in here\n");
+	}
+	else if (curr.quote == 0)
+	{
+		if (ft_dollar(argv[i]))
+		{
+			argv[i] = strremove(argv[i], "$");
+			show_var(argv[i], head_ref);
+		}
+		else 
+			print_echo_args(argv, i);
 	}
 	else
-	{
 		print_echo_args(argv, i);
-	}
 	if (flag == 0)
 		printf("\n");
 	return (0);
