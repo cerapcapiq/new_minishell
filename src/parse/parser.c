6cfe79bf7a813628fb5c5f03bcb7a34d454be3e5
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-16 15:50:49 +0800

    Add files via upload

--- src/parse/parser.c
+++ src/parse/parser.c
@@ -6,12 +6,135 @@
 /*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 13:15:41 by abasarud          #+#    #+#             */
-/*   Updated: 2023/04/18 14:43:22 by abasarud         ###   ########.fr       */
+/*   Updated: 2023/05/16 15:49:59 by abasarud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../libft/libft.h"
+#include <histedit.h>
+#include <string.h>
+
+/*char** splitString(const char* input, int* tokenCount) {
+    const int maxTokens = 100;  // Maximum number of tokens
+    char** result = malloc(maxTokens * sizeof(char*));
+    char* current = malloc(strlen(input) + 1);
+    int currentIndex = 0;
+    int insideQuotes = 0;
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        char c = input[i];
+
+        if (c == ' ' && !insideQuotes) {
+            if (currentIndex > 0) {
+                current[currentIndex] = '\0';
+                result[*tokenCount] = malloc(strlen(current) + 1);
+                strcpy(result[*tokenCount], current);
+                (*tokenCount)++;
+                currentIndex = 0;
+            }
+        } else if (c == '\"' || c == '\'') {
+            insideQuotes = !insideQuotes;
+        } else {
+            current[currentIndex] = c;
+            currentIndex++;
+        }
+    }
+
+    if (currentIndex > 0) {
+        current[currentIndex] = '\0';
+        result[*tokenCount] = malloc(strlen(current) + 1);
+        strcpy(result[*tokenCount], current);
+        (*tokenCount)++;
+    }
+
+    free(current);
+    return result;
+}
+
+void freeTokens(char** tokens, int tokenCount) {
+    for (int i = 0; i < tokenCount; i++) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
+*/
+char** split_string(const char* string) {
+    int max_tokens = 100; // Maximum number of tokens
+    int token_count = 0; // Current token count
+    char** tokens = malloc(max_tokens * sizeof(char*)); // Array to store tokens
+    char* current_word = NULL; // Current token being processed
+    int in_quotes = 0; // Flag to indicate if within quotes
+    char quote_type = '\0'; // Single or double quote
+
+    if (!tokens) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+
+    // Tokenize the string
+    while (*string) {
+        if ((*string == ' ' && !in_quotes) || *string == '"' || *string == '\'') {
+            if (current_word) {
+                tokens[token_count++] = current_word;
+                current_word = NULL;
+
+                // Resize the tokens array if necessary
+                if (token_count >= max_tokens) {
+                    max_tokens *= 2;
+                    tokens = realloc(tokens, max_tokens * sizeof(char*));
+                    if (!tokens) {
+                        fprintf(stderr, "Memory reallocation failed.\n");
+                        free(current_word);
+                        return NULL;
+                    }
+                }
+            }
+
+            if (*string == '"' || *string == '\'') {
+                if (in_quotes && quote_type == *string) {
+                    in_quotes = 0;
+                    quote_type = '\0';
+                }
+                else if (!in_quotes) {
+                    in_quotes = 1;
+                    quote_type = *string;
+                }
+            }
+        }
+        else {
+            if (!current_word) {
+                // Allocate memory for the current token
+                current_word = malloc(strlen(string) + 1);
+                if (!current_word) {
+                    fprintf(stderr, "Memory allocation failed.\n");
+                    free(tokens);
+                    return NULL;
+                }
+                strcpy(current_word, "");
+            }
+            size_t length = strlen(current_word);
+            current_word = realloc(current_word, (length + 2) * sizeof(char));
+            current_word[length] = *string;
+            current_word[length + 1] = '\0';
+        }
+
+        string++;
+    }
+    if (current_word) {
+        tokens[token_count++] = current_word;
+    }
+    tokens = realloc(tokens, token_count * sizeof(char*));
+
+    return tokens;
+    free(tokens);
+}
+void free_tokens(char** tokens, int tokenCount) {
+    for (int i = 0; i < tokenCount; i++) {
+        free(tokens[i]);
+    }
+    //free(tokens);
+}
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -25,26 +148,49 @@ int	ft_strcmp(char *s1, char *s2)
 
 void	ft_whitespace(char *buff)
 {
-	while (*buff == ' ' || *buff == '\t')
+	while (*buff == '\t' || *buff == ' ' )
 		buff++;
 	if (*buff == '\0')
-		mini_exit();
+		exit(0);
+}
+char* add_spaces_around_pipe(const char* s)
+{
+    size_t len = ft_strlen(s);
+    char* result = malloc(sizeof(char) * (len * 2 + 1));
+    if (!result) {
+        return NULL;
+    }
+
+    size_t j = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (s[i] == '|' && (i == 0 || s[i-1] != ' ') && (i == len - 1 || s[i+1] != ' ')) {
+            result[j++] = ' ';
+            result[j++] = '|';
+            result[j++] = ' ';
+        } else {
+            result[j++] = s[i];
+        }
+    }
+    result[j] = '\0';
+
+    return result;
 }
 
 void	parse(t_mini *mini, char *buff)
 {
 	char	**split;
 	int		i;
-	t_token	*head;
+	t_token	*head = NULL;
 	char	*cpy;
 
 	split = NULL;
 	if (buff == NULL)
 		exit(0);
-	ft_whitespace(buff);
+    buff = add_spaces_around_pipe(buff);
 	cpy = ft_strdup(buff);
 	ft_linked_list(cpy);
-	split = ft_split(buff, ' ');
+	ft_whitespace(cpy);
+	split = ft_split(cpy, ' ');
 	head = new_token(mini, *split);
 	mini->tokens = head;
 	i = 0;
